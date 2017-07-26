1661ab739782bd16d73c27d33addf353ca50a05c
Renan Gonçalves <renan.saddam@gmail.com>
2017-07-26 11:47:16 +0200

    Only override the methods we need to measure time

--- src/Doctrine/PDOConnection.php
+++ src/Doctrine/PDOConnection.php
@@ -2,116 +2,60 @@
 
 namespace Werkspot\Pinba\Doctrine;
 
-/*
- * This is native Doctrine\DBAL\Driver\PDOConnection, the only difference is that
- * i's extended from PDOPinba instead of regular PDO
- */
-use Doctrine\DBAL\Driver\Connection;
-use Doctrine\DBAL\Driver\ServerInfoAwareConnection;
-use PDOException;
+use Doctrine\DBAL\Driver\PDOConnection as DBALConnection;
 use Werkspot\Pinba\PDO\PDO as PinbaPDO;
+use Werkspot\Pinba\PinbaTimer;
 
-class PDOConnection extends PinbaPDO implements Connection, ServerInfoAwareConnection
+class PDOConnection extends DBALConnection
 {
-    /**
-     * @param string      $dsn
-     * @param string|null $user
-     * @param string|null $password
-     * @param array|null  $options
-     *
-     * @throws PDOException in case of an error
-     */
-    public function __construct($dsn, $user = null, $password = null, array $options = null)
+    public function __construct(...$arguments)
     {
-        try {
-            parent::__construct($dsn, $user, $password, $options);
-            $this->setAttribute(PinbaPDO::ATTR_STATEMENT_CLASS, ['Doctrine\DBAL\Driver\PDOStatement', []]);
-            $this->setAttribute(PinbaPDO::ATTR_ERRMODE, PinbaPDO::ERRMODE_EXCEPTION);
-        } catch (\PDOException $exception) {
-            throw new PDOException($exception);
-        }
+        $tags = ['group' => 'mysql', 'op' => 'connect'];
+        $timer = PinbaTimer::start($tags);
+        $result = call_user_func_array(['parent', '__construct'], $arguments);
+        $timer->stop();
+
+        return $result;
     }
 
-    /**
-     * {@inheritdoc}
-     */
+    public function beginTransaction()
+    {
+        $tags = ['group' => 'mysql', 'op' => 'begin'];
+        $timer = PinbaTimer::start($tags);
+        $result = parent::beginTransaction();
+        $timer->stop();
+
+        return $result;
+    }
+
+    public function commit()
+    {
+        $tags = ['group' => 'mysql', 'op' => 'commit'];
+        $timer = PinbaTimer::start($tags);
+        $result = parent::commit();
+        $timer->stop();
+
+        return $result;
+    }
+
+    public function rollBack()
+    {
+        $tags = ['group' => 'mysql', 'op' => 'rollback'];
+        $timer = PinbaTimer::start($tags);
+        $result = parent::rollBack();
+        $timer->stop();
+
+        return $result;
+    }
+
     public function exec($statement)
     {
-        try {
-            return parent::exec($statement);
-        } catch (\PDOException $exception) {
-            throw new PDOException($exception);
-        }
-    }
+        $tags = ['group' => 'mysql', 'op' => PinbaPDO::getQueryType($statement)];
+        $data = ['sql' => $statement];
+        $timer = PinbaTimer::start($tags, $data);
+        $result = parent::exec($statement);
+        $timer->stop();
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getServerVersion()
-    {
-        return PinbaPDO::getAttribute(PinbaPDO::ATTR_SERVER_VERSION);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function prepare($prepareString, array $driverOptions = [])
-    {
-        try {
-            return parent::prepare($prepareString, $driverOptions);
-        } catch (\PDOException $exception) {
-            throw new PDOException($exception);
-        }
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function query()
-    {
-        $args = func_get_args();
-        $argsCount = count($args);
-
-        try {
-            if ($argsCount == 4) {
-                return parent::query($args[0], $args[1], $args[2], $args[3]);
-            }
-
-            if ($argsCount == 3) {
-                return parent::query($args[0], $args[1], $args[2]);
-            }
-
-            if ($argsCount == 2) {
-                return parent::query($args[0], $args[1]);
-            }
-
-            return parent::query($args[0]);
-        } catch (\PDOException $exception) {
-            throw new PDOException($exception);
-        }
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function quote($input, $type = PinbaPDO::PARAM_STR)
-    {
-        return parent::quote($input, $type);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function lastInsertId($name = null)
-    {
-        return parent::lastInsertId($name);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function requiresQueryForServerVersion()
-    {
-        return false;
+        return $result;
     }
 }
